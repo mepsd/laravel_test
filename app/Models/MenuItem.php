6fc880ac50497552a3cd23a9cf2d68624b14ad79
Paras Suthar Darji <parassuthardarji@gmail.com>
2022-06-03 01:16:08 +0530

    Recursive menu tree added by relation

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    public function submenu()
+    {
+        return $this->hasMany(MenuItem::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->submenu()->with('children');
+    }
+
+  
 
 }
